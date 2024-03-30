07f7ac87b7e8393366b3f78d17775c2c73527650
Chhat <chhatseh@gmail.com>
2024-03-30 17:48:13 +0800

    making parameters all above 77 and equalize it

--- Script/script.py
+++ Script/script.py
@@ -3,38 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 
-def convert_to_grayscale(image):
-    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-def apply_noise_reduction(image, method='gaussian', kernel_size=7):
-    if method == 'gaussian':
-        return cv2.GaussianBlur(image, (kernel_size, kernel_size), 5)
-    else:
-        raise ValueError("Unknown noise reduction method.")
-
-def equalize_histogram(image):
-    return cv2.equalizeHist(image)
-
-def threshold_image(image, method='otsu'):
-    if method == 'otsu':
-        _, binary_image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-        return binary_image
-    else:
-        raise ValueError("Unknown thresholding method.")
-
 def invert_colors(image):
     return cv2.bitwise_not(image)
-
-def adaptive_threshold_image(image):
-    return cv2.adaptiveThreshold(
-        image,
-        255,
-        cv2.ADAPTIVE_THRESH_MEAN_C,
-        cv2.THRESH_BINARY,
-        blockSize=1,  # Size of a pixel neighborhood that is used to calculate a threshold value
-        C=2  # Constant subtracted from the mean or weighted mean
-    )
-
 
 def calculate_miou(prediction, target, target_original_shape):
     # Resize target to match prediction's shape if they differ
@@ -49,10 +19,6 @@ def calculate_miou(prediction, target, target_original_shape):
     return iou_score
 
 # New function to convert to HSV and split
-def convert_to_hsv_and_split(image):
-    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    h, s, v = cv2.split(hsv_image)
-    return h, s, v
 
 # Placeholder for morphological transformations
 def apply_morphological_operations(image, close_kernel_size=3, open_kernel_size=3):
@@ -68,91 +34,14 @@ def apply_morphological_operations(image, close_kernel_size=3, open_kernel_size=
 
     return opening
 
-def segment_flower_using_color(image):
-    # Convert to HSV color space
-    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-    # Define the color range for yellow flowers
-    lower_yellow = np.array([20, 100, 100])
-    upper_yellow = np.array([30, 255, 255])
-
-    # Create a mask for the yellow color
-    mask = cv2.inRange(hsv_image, lower_yellow, upper_yellow)
-
-    # Apply morphological operations to clean up the mask
-    kernel = np.ones((5, 5), np.uint8)
-    clean_mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-    clean_mask = cv2.morphologyEx(clean_mask, cv2.MORPH_OPEN, kernel)
-
-    return clean_mask
-
 def apply_bilateral_filter(image, d=9, sigma_color=75, sigma_space=75):
-
-    """
-
-    Apply bilateral filtering to reduce noise while preserving edges.
-
-
-
-    Parameters:
-
-    - image: The input image
-
-    - d: Diameter of each pixel neighborhood used during filtering
-
-    - sigma_color: Value for filter sigma in the color space
-
-    - sigma_space: Value for filter sigma in the coordinate space
-
-
-
-    Returns:
-
-    - The filtered image
-
-    """
-
     return cv2.bilateralFilter(image, d, sigma_color, sigma_space)
 
-def adjust_gain(image, gain=1.0):
-    """
-    Adjusts the gain (brightness) of an image.
-
-    Parameters:
-    - image: Input image
-    - gain: Factor by which to multiply pixel values
-
-    Returns:
-    - The brightness adjusted image
-    """
-    # Convert to float to avoid clipping during multiplication
-    f_image = image.astype(np.float32)
-    # Multiply the image by the gain factor
-    f_image = cv2.multiply(f_image, np.array([gain]))
-    # Clip values to the range [0, 255] and convert back to uint8
-    adjusted_image = np.clip(f_image, 0, 255).astype(np.uint8)
-    return adjusted_image
-
-def gamma_correction(image, gamma=1.0):
-    """
-    Performs gamma correction on an image.
-
-    Parameters:
-    - image: Input image
-    - gamma: Gamma value for correction
-
-    Returns:
-    - The gamma corrected image
-    """
-    # Build a lookup table mapping pixel values [0, 255] to their adjusted gamma values
-    invGamma = 1.0 / gamma
-    table = np.array([((i / 255.0) ** invGamma) * 255 for i in np.arange(256)]).astype(np.uint8)
-
-    # Apply gamma correction using the lookup table
-    return cv2.LUT(image, table)
-
-
 def apply_kmeans(image, K=2):
+    # Check if the image is grayscale. If so, convert it to a 3-channel format.
+    if len(image.shape) == 2:
+        image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+
     # Convert image into a feature vector
     pixel_values = image.reshape((-1, 3))
     pixel_values = np.float32(pixel_values)
@@ -161,94 +50,18 @@ def apply_kmeans(image, K=2):
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
     _, labels, (centers) = cv2.kmeans(pixel_values, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
 
-    # Convert back to 8 bit values
-    centers = np.uint8(centers)
+    # Assuming that we are interested in the brighter cluster (flower is typically brighter)
+    # Compute the mean intensity of each cluster and select the brighter one as the flower
+    mean_intensity = np.mean(centers, axis=1)
+    flower_label = np.argmax(mean_intensity)
 
-    # Map labels to center values
-    segmented_image = centers[labels.flatten()]
-    segmented_image = segmented_image.reshape(image.shape)
-
-    # Assuming the flower is brighter, find which cluster is brighter
-    brightness = np.sum(centers, axis=1)
-    flower_cluster = np.argmax(brightness)
-
-    # Create a mask where the brighter cluster is white, and the other is black
-    mask = np.where(labels == flower_cluster, 255, 0)
-    mask = mask.astype(np.uint8)
+    # Construct the mask based on the identified flower cluster
+    mask = np.zeros(labels.shape, dtype=np.uint8)
+    mask[labels == flower_label] = 255
     mask = mask.reshape((image.shape[0], image.shape[1]))
 
     return mask
 
-def select_roi(image, x, y, w, h):
-    """
-    Selects a region of interest (ROI) from an image.
-
-    Parameters:
-    - image: The original image.
-    - x: The x-coordinate of the top-left corner of the ROI.
-    - y: The y-coordinate of the top-left corner of the ROI.
-    - w: The width of the ROI.
-    - h: The height of the ROI.
-
-    Returns:
-    - roi: The selected region of interest from the image.
-    """
-    roi = image[y:y+h, x:x+w]
-    return roi
-
-
-def insert_roi_into_full_image(full_image, roi_image, x, y, w, h):
-    """
-    Inserts the processed ROI back into the full image.
-
-    Parameters:
-    - full_image: The original full-sized image.
-    - roi_image: The processed ROI.
-    - x, y: The top-left coordinates from which the ROI was extracted.
-    - w, h: The width and height of the ROI.
-    """
-    full_image[y:y+h, x:x+w] = roi_image
-    return full_image
-
-def find_roi_contours(image, lower_hue, lower_saturation, lower_value, upper_hue, upper_saturation, upper_value):
-    # Convert image to HSV color space
-    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-    # Define color range for segmentation based on the parameters
-    lower_bound = np.array([lower_hue, lower_saturation, lower_value])
-    upper_bound = np.array([upper_hue, upper_saturation, upper_value])
-
-    # Threshold the image to get only the colors in the defined range
-    mask = cv2.inRange(hsv_image, lower_bound, upper_bound)
-
-    # Find contours in the mask
-    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    return contours
-
-
-def detect_flower_color(image):
-    # Convert to HSV color space
-    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-    # Define ranges for yellow and white colors
-    yellow_lower = np.array([20, 100, 100], np.uint8)
-    yellow_upper = np.array([30, 255, 255], np.uint8)
-    white_lower = np.array([0, 0, 200], np.uint8)
-    white_upper = np.array([180, 55, 255], np.uint8)
-
-    # Threshold the HSV image to get only the colors within the range
-    yellow_mask = cv2.inRange(hsv_image, yellow_lower, yellow_upper)
-    white_mask = cv2.inRange(hsv_image, white_lower, white_upper)
-
-    # Compare the number of pixels for each color
-    yellow_count = cv2.countNonZero(yellow_mask)
-    white_count = cv2.countNonZero(white_mask)
-
-    if yellow_count >= white_count:
-        return 'yellow', yellow_mask
-    else:
-        return 'white', white_mask
 
 # https://publisher.uthm.edu.my/periodicals/index.php/eeee/article/view/441
 # not sure it does anything
@@ -262,12 +75,14 @@ def process_and_compare_image(input_path, ground_truth_path):
     # Load the image
     image = cv2.imread(input_path)
 
-    # Proceed with bilateral filtering or other image processing steps
+    # Proceed with bilateral filtering
     bilateral_filtered_image = apply_bilateral_filter(image)
 
-    # Apply median filtering
-    median_filtered_image = apply_median_filter(bilateral_filtered_image, kernel_size=9)
+    # Convert image to grayscale
+    grayscale_image = cv2.cvtColor(bilateral_filtered_image, cv2.COLOR_BGR2GRAY)
 
+    # Apply median filtering
+    median_filtered_image = apply_median_filter(grayscale_image, kernel_size=9)
 
     # Apply K-means clustering for segmentation
     kmeans_result = apply_kmeans(median_filtered_image)
@@ -289,8 +104,10 @@ def process_and_compare_image(input_path, ground_truth_path):
     miou_score = calculate_miou(morph_result // 255, inverted_ground_truth // 255, inverted_ground_truth.shape)
 
     # Collect images for comparison
-    images = [image, bilateral_filtered_image, kmeans_result, inverted_ground_truth, morph_result]
-    descriptions = ["Original Image", "Preprocessed Image", "K-means Result", "Inverted Ground Truth", "Morphology"]
+    images = [image, bilateral_filtered_image, grayscale_image, median_filtered_image, kmeans_result, inverted_ground_truth,
+              morph_result]
+    descriptions = ["Original Image", "Bilateral Filtered", "Grayscale", "Median Filtered", "K-Means", "Inverted Ground Truth",
+                    "Morphology"]
 
     return miou_score, images, descriptions
 
